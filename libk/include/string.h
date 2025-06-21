b0f6d8611c733af97c00c939d1ededec19ea37ed
atomicul <soaremihai2006@gmail.com>
2025-06-21 22:25:12 +0300

    Add more string functions to libk

---
+++ libk/include/string.h
@@ -0,0 +1,11 @@
+#ifndef _STRING_H
+#define _STRING_H
+
+#include <stddef.h>
+
+void *memcpy(void *dest, const void *src, size_t count);
+char *strcpy(char *dest, const char *src);
+char *strcat(char *dest, const char *src);
+size_t strlen(const char *);
+
+#endif
