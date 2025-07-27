e8b4eb03ecaf0c4dac588e560fd59a0ba16678f4
atomicul <soaremihai2006@gmail.com>
2025-07-27 16:30:44 +0300

    Print paging demo text

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,9 @@
+#include <kernel/memmap.h>
+
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #if defined(__linux__)
 #error "This code must be compiled with a cross-compiler"
@@ -8,6 +11,20 @@
 #error "This code must be compiled with an x86-elf compiler"
 #endif
 
+uint8_t byte;
+
 void kernel_main(void) {
-    puts("I'm in the higher half :D");
+    printf("Random static kernel variable address: 0x%x\n", &byte);
+
+    const char *message = "I'm in the higher half :D";
+    char* memory = memmap(strlen(message)+1);
+
+    strcpy(memory, message);
+
+    puts(memory);
+
+    printf("Message physical address: 0x%x\n", memphys(memory));
+    printf("Message virtual address: 0x%x\n", memory);
+
+    memfree(memory);
 }
