4d1754eb43c719f053c0007d4ddf13ed6a1cf9c2
atomicul <soaremihai2006@gmail.com>
2025-07-27 14:20:15 +0300

    Implement memfree function

--- kernel/arch/i386/memmap.c
+++ kernel/arch/i386/memmap.c
@@ -1,9 +1,12 @@
 #include <kernel/page_directory.h>
 #include <kernel/page_table.h>
+#include <kernel/panic.h>
 #include <kernel/physical_memory_allocator.h>
 #include <kernel/node_allocator.h>
 
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 
 #define KERNEL_START 0xC0000000
 #undef NULL
@@ -18,6 +21,27 @@ typedef struct FreeSpace {
 } FreeSpace;
 
 FreeSpace *free_list;
+
+
+typedef struct AllocatedSpace {
+    size_t size;
+} AllocatedSpace;
+
+AllocatedSpace alloc_table[KERNEL_START/FRAME_SIZE];
+
+static inline void compute_page_alloc(size_t *bytes, size_t *page_count, size_t *huge_page_count) {
+    *huge_page_count = *bytes / HUGE_FRAME_SIZE;
+    *bytes -= *huge_page_count * HUGE_FRAME_SIZE;
+
+    *page_count = *bytes / FRAME_SIZE;
+    *bytes -= *page_count * FRAME_SIZE;
+
+    if (*bytes > 0) {
+        (*page_count)++;
+    }
+
+    *bytes = *huge_page_count*HUGE_FRAME_SIZE + *page_count*FRAME_SIZE;
+}
 
 static inline uintptr_t get_free_space(size_t bytes, size_t alignment) {
     for (FreeSpace *i = free_list, *prev = NULL;
@@ -79,18 +103,55 @@ static inline uintptr_t get_free_space(size_t bytes, size_t alignment) {
     return NULL;
 }
 
-void *memmap(size_t bytes) {
-    size_t huge_page_count = bytes / HUGE_FRAME_SIZE;
-    bytes -= huge_page_count * HUGE_FRAME_SIZE;
+static inline void reclaim_free_space(uintptr_t addr, size_t size) {
+    FreeSpace *i = free_list, *prev = NULL;
+    for (; i != NULL || prev != NULL;
+         prev=i, i=prev == NULL ? NULL : prev->next) {
 
-    size_t page_count = bytes / FRAME_SIZE;
-    bytes -= page_count * FRAME_SIZE;
+        bool aligned_behind = prev == NULL || prev->address + prev->size <= addr;
+        bool aligned_ahead = i == NULL || addr + size <= i->address;
 
-    if (bytes > 0) {
-        page_count++;
+        if (aligned_behind && aligned_ahead) {
+            break;
+        }
     }
 
-    bytes = huge_page_count*HUGE_FRAME_SIZE + page_count*FRAME_SIZE;
+    // Insert node here
+    FreeSpace *new_node = nalloc_get_node(nodes_buff);
+
+    if (prev == NULL) {
+        free_list = new_node;
+    } else {
+        prev->next = new_node;
+    }
+
+    new_node->address = addr;
+    new_node->size = size;
+    new_node->next = i;
+
+    i = new_node;
+
+    if (prev != NULL && prev->address + prev->size >= i->address) {
+        prev->next = i->next;
+        prev->size = i->address + i->size - prev->address;
+
+        nalloc_free_node(nodes_buff, i);
+        i = prev;
+        prev = NULL;
+    }
+
+    if (i->next != NULL && i->address + i->size >= i->next->address) {
+        FreeSpace *p = i->next;
+        i->next = p->next;
+        i->size = p->address + p->size - i->address;
+
+        nalloc_free_node(nodes_buff, p);
+    }
+}
+
+void *memmap(size_t bytes) {
+    size_t page_count, huge_page_count;
+    compute_page_alloc(&bytes, &page_count, &huge_page_count);
 
     const uintptr_t address = get_free_space(bytes, huge_page_count > 0 ? HUGE_FRAME_SIZE : FRAME_SIZE);
 
@@ -120,7 +181,42 @@ void *memmap(size_t bytes) {
         *table_entry |= PDE_WRITE_ENABLED | PDE_PRESENT;
     }
 
+    alloc_table[address/FRAME_SIZE].size = bytes;
     return (void*)address;
+}
+
+void memfree(void *address) {
+    AllocatedSpace *entry = alloc_table + (uintptr_t)address/FRAME_SIZE;
+
+    if (entry->size == 0) {
+        panic("[memfree] Attempted to free already free memory");
+    }
+
+    size_t pages, huge_pages;
+    compute_page_alloc(&entry->size, &pages, &huge_pages);
+
+    uintptr_t normal_pages_start = (uintptr_t)address + HUGE_FRAME_SIZE*huge_pages;
+
+    for (PDE *dir_entry = pde_get_by_linear_address((uintptr_t)address);
+            huge_pages--;
+            dir_entry++) {
+
+        pma_free_frame(*dir_entry >> 22 << 22);
+
+        *dir_entry = 0;
+    }
+
+    for (PTE *table_entry = pte_get_by_linear_address(normal_pages_start);
+            pages--;
+            table_entry++) {
+
+        pma_free_frame(*table_entry >> 12 << 12);
+
+        *table_entry = 0;
+    }
+
+    reclaim_free_space((uintptr_t)address, entry->size);
+    entry->size = 0;
 }
 
 void mem_init(void) {
@@ -132,3 +228,4 @@ void mem_init(void) {
     free_list->address = HUGE_FRAME_SIZE;
     free_list->size = KERNEL_START  - free_list->address;
 }
+
